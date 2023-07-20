00490b67acac1bf6b85dfa29bf50e1d66358ed15
NCooper04 <47705652+NCooper04@users.noreply.github.com>
2023-07-20 11:54:36 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,45 @@
-    # Importing urllib request module in the program  
-from webbrowser import open
+ # Importing GUI functions
 
-    # Importing GUI functions
-from tkinter import *
+from tkinter import Tk, Button, Label
 
     # Importing CSV reader
-from pandas import csv
 
-    # Importing youtube URL generating functions
-import random 
-import string
+from csv import reader
+from random import choice
+import webbrowser
 
-characters = string.ascii_letters + "_" + '-' + string.digits
+fileName = ('C:\\Users\\u7144140\\Downloads\\videos.csv')
+file = open(fileName)
 
 top = Tk()
-label = Label(text="Bored?")
+label = Label(text='Bored?')
 label.pack()
 
-top.geometry("150x100")
+top.geometry('250x200')
 
-def wiki():  
-    # Using urlopen() function with url in it  
+
+def wiki():
+
+    # Using urlopen() function with url in it
+
     webUrl = 'https://en.wikipedia.org/wiki/Special:Random'
     open(webUrl)
 
-def tube(): 
-    videoID = ''.join(random.choice(characters) for i in range (11))
-    webUrl = "https://www.youtube.com/watch?v=" + videoID
-    open(webUrl)
 
+def tube():
+    csv_reader = reader(file)
+    
+    data = [r for r in csv_reader]
+    videoID = choice(data)[0]
+    webUrl = 'https://www.youtube.com/watch?v=' + videoID[2:13]
+    webbrowser.open(webUrl)
 
 b1 = Button (top, text = "Wiki page?", command=wiki, activeforeground="red", activebackground="pink")
-b1.pack(side=LEFT)
 
-b2 = Button (top, text = "Youtube link?", command=tube, activeforeground="red", activebackground="pink")
-b2.pack(side=RIGHT)
+b1.pack(side="top")
+
+b2 = Button(top, text='Youtube link?', command=tube,
+            activeforeground='red', activebackground='pink')
+b2.pack(side="bottom")
 
 top.mainloop()
